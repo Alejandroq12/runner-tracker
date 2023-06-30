9fe67bc0c1106393ceea65ff013899461413b8ae
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 12:58:42 -0600

    Add logo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
   );
---
+++ src/components/logo/Logo.js
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+      <div className='ma4 mt0'>
+        
+      </div>
+    );
+}
+
+export default Logo;
