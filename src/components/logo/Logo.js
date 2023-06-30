dd4274affe9e0fff4e6c11ca3a954ff959d5e3b0
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 13:46:58 -0600

    Add Parallax effect

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,10 +1,11 @@
 import Tilt from 'react-parallax-tilt';
+import './Logo.css';
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt>
-        <div options={{ max : 155 }} style={{ height: '150px', width: '150px', backgroundColor: 'darkgreen' }}>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 155 }} style={{ height: '150px', width: '150px' }} >
+        <div>
           <h1>React Parallax Tilt 👀</h1>
         </div>
       </Tilt>
