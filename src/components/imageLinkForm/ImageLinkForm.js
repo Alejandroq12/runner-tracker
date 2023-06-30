1de5556be9be0b1aa59d601a4c7cc92b23764b67
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 14:26:44 -0600

    Add structure of the Image Link Component"

---
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -0,0 +1,14 @@
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <p className="f3">
+        {' '}
+        {
+          'This Smart Recognition App will detect faces in your pictures. Give it a try.'
+        }{' '}
+      </p>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
