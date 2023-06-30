c75c08b7d4e80c2603287bfd6759d2b55a5c4792
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 14:43:15 -0600

    Add input form

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -7,6 +7,10 @@ const ImageLinkForm = () => {
           'This Smart Recognition App will detect faces in your pictures. Give it a try.'
         }{' '}
       </p>
+      <div>
+        <input className='f4 pa2 w-70 center' type="tex" />
+        <button className='w-30 grow link ph3 pv2 dib white bg-light-purple'>Detect</button>
+      </div>
     </div>
   );
 };
