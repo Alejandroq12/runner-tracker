a0e665d0950b4473e88aea471a2ae3a54e139544
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 15:03:28 -0600

    Add background and increase size of input form

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,3 +1,5 @@
+import './ImageLinkForm.css';
+
 const ImageLinkForm = () => {
   return (
     <div>
@@ -8,7 +10,7 @@ const ImageLinkForm = () => {
         }{' '}
       </p>
       <div className="center">
-        <div className='pa4 br3 shadow-5'>
+        <div className='form pa4 br3 shadow-5'>
           <input className="f4 pa2 w-70 center" type="tex" />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
             Detect
