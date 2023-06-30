a794c0fc9a296951d6cf893a5202f6e864dcc013
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 15:19:10 -0600

    Add Rank component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,9 @@
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{'Your current rank is...'}</div>
+    </div>
+  );
+};
+
+export default Rank;
