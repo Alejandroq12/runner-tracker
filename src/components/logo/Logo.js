f2f17e627bfed80ecf6671e23a23dbe0d1cf326d
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 14:08:00 -0600

    Fix code indentation and add padding to logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,9 +5,15 @@ import logo from './logo.png';
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 155 }} style={{ height: '150px', width: '150px' }} >
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 155 }}
+        style={{ height: '150px', width: '150px' }}
+      >
         <div>
-          <div className='Tilt-inner pa3'><img alt='logo' src={logo} /> </div>
+          <div className="Tilt-inner pa3">
+            <img style={{ paddingTop: '6px' }} alt="logo" src={logo} />{' '}
+          </div>
         </div>
       </Tilt>
     </div>
