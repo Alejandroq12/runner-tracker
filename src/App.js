2bd8782073a468a1c56f59e3abc8959e24877113
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 16:19:47 -0600

    Update particles effect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import ParticlesBg from 'particles-bg';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg num={300} type="lines" bg />
       <Navigation />
       <Logo />
       <Rank />
