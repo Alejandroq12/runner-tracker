bf3c789658d58b97b41ac0c96fb523e38d041fce
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-30 14:56:33 -0600

    Add style to the input form

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -7,9 +7,13 @@ const ImageLinkForm = () => {
           'This Smart Recognition App will detect faces in your pictures. Give it a try.'
         }{' '}
       </p>
-      <div>
-        <input className='f4 pa2 w-70 center' type="tex" />
-        <button className='w-30 grow link ph3 pv2 dib white bg-light-purple'>Detect</button>
+      <div className="center">
+        <div className='pa4 br3 shadow-5'>
+          <input className="f4 pa2 w-70 center" type="tex" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
       </div>
     </div>
   );
