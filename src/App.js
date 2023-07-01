41e99b246612790767599936413262c975286d94
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 17:07:34 -0600

    Get the width and height of faces detected

--- src/App.js
+++ src/App.js
@@ -53,6 +53,8 @@ class App extends Component {
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
   }
 
   onInputChange = (event) => {
