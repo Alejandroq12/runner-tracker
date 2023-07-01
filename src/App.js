8e5a713ade0ab15212e87259aed41b2889715064
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 17:04:47 -0600

    Get image by ID

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
   }
 
   onInputChange = (event) => {
