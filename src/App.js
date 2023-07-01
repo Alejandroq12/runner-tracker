1748ecea2f64db8b269b8cbb3b62cbebb66960c6
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 13:22:07 -0600

    Add logic to call API

--- src/App.js
+++ src/App.js
@@ -19,7 +19,45 @@ class App extends Component {
   };
 
   onButtonSubmit = () => {
-    console.log('click');
+    // URL of image to use. Change this to your image.
+    const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: 'clarifai',
+        app_id: 'main',
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Key ' + 'eb4da108907e425c9b10c1897fffaa99',
+      },
+      body: raw,
+    };
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+
+    fetch(
+      `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
   };
 
   render() {
