4f22d02a7d7044edfcc9d3d9373f163ba16df7b7
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 12:58:58 -0600

    Add on click event

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    console.log(event);
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
   };
 
   render() {
@@ -25,7 +29,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange }) => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3 white">
@@ -10,9 +10,16 @@ const ImageLinkForm = ({ onInputChange }) => {
         }{' '}
       </p>
       <div className="center">
-        <div className='form center pa4 br3 shadow-5'>
-          <input className="f4 pa2 w-70 center" type="tex" onChange={onInputChange} />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            className="f4 pa2 w-70 center"
+            type="tex"
+            onChange={onInputChange}
+          />
+          <button
+            onClick={onButtonSubmit}
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+          >
             Detect
           </button>
         </div>
