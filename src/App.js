737fdd1bf4c37fe0909513ef19ea24a78b5c98ee
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 16:20:48 -0600

    Setup API

--- src/App.js
+++ src/App.js
@@ -7,6 +7,39 @@ import Rank from './components/rank/Rank';
 import ParticlesBg from 'particles-bg';
 import './App.css';
 
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const PAT = 'eb4da108907e425c9b10c1897fffaa99';
+  const USER_ID = 'aleq12';
+  const APP_ID = 'my-first-application-eye2ra';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
+  return requestOptions;
+};
+
 class App extends Component {
   constructor() {
     super();
@@ -17,50 +50,17 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value })
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    // URL of image to use. Change this to your image.
-    const IMAGE_URL = this.state.input;
-
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: 'clarifai',
-        app_id: 'main',
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: IMAGE_URL,
-            },
-          },
-        },
-      ],
-    });
-
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        Authorization: 'Key ' + 'eb4da108907e425c9b10c1897fffaa99',
-      },
-      body: raw,
-    };
-
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
-
     fetch(
       `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
-      requestOptions
+      returnClarifaiRequestOptions(this.state.input)
     )
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log('error', error));
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   };
 
   render() {
