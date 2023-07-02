089d27a1b7598d21766ddcfaa3a8372d4399129d
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 22:21:14 -0600

    Add field set in Sign In component

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -3,7 +3,32 @@ const Signin = () => {
     <article className="br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white-60">
       <main className="pa4 black-80">
         <form className="measure">
-
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" for="email-address">
+                Email
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" for="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+          </fieldset>
+        
         </form>
       </main>
     </article>
