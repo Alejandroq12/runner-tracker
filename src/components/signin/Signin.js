12655a94d7e584f7353dded974fffd7fbdad1b9d
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 22:22:25 -0600

    Add sign in button and register link to Sign In component

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -28,7 +28,18 @@ const Signin = () => {
               />
             </div>
           </fieldset>
-        
+          <div className="">
+            <input
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Sign in"
+            />
+          </div>
+          <div className="lh-copy mt3">
+            <a href="#0" className="f6 link dim black db">
+              Register
+            </a>
+          </div>
         </form>
       </main>
     </article>
