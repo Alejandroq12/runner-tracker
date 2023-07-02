936b3c65ba403a5f9e819b89f4439db45e9d657f
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-01 22:20:39 -0600

    Add form in Sign In component

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -1,7 +1,11 @@
 const Signin = () => {
   return (
     <article className="br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white-60">
+      <main className="pa4 black-80">
+        <form className="measure">
 
+        </form>
+      </main>
     </article>
   );
 };
