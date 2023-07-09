195f333cf6396074b0b080850ddb06e79a260671
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-08 18:45:27 -0600

    Connect backend

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -17,7 +17,14 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    console.log(this.state);
+    fetch('http://localhost:3003/signin', {
+      method: 'post',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    });
     this.props.onRouteChange('home');
   };
 
