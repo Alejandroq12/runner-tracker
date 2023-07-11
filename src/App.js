a5dfcfa66d935f3dd31d54662ef2477d9b4d7c71
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-10 21:11:22 -0600

    Pass user.name and user.entries into Rank component as props

--- src/App.js
+++ src/App.js
@@ -135,7 +135,10 @@ class App extends Component {
         {route === 'home' ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
