e1cc85651ba8a4a328e4410d551f8570081f9003
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-11 21:31:39 -0600

    Add string literals

--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -1,7 +1,7 @@
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className="white f3">{'Julio, your current rank is...'}</div>
+      <div className="white f3">{`${name}, your current entry count is...`}</div>
       <div className="white f1">{'#4'}</div>
     </div>
   );
