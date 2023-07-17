1a887a1df8c211ad506e9ab4555022b608d4d6af
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-16 19:38:28 -0600

    Implement Object.assign

--- src/App.js
+++ src/App.js
@@ -118,11 +118,7 @@ class App extends Component {
           })
             .then((response) => response.json())
             .then((count) => {
-              this.setState({
-                user: {
-                  entries: count,
-                },
-              });
+              this.setState(Object.assign(this.state.user, { entries: count }));
             });
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
