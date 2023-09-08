fab3834e382efcf624538cafb67a3fcf3678fda0
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-08 13:13:13 -0600

    Render image conditionally

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -4,13 +4,15 @@ const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img
-          id="inputImage"
-          alt="Input file to detect face"
-          src={imageUrl}
-          width="500px"
-          height="auto"
-        />
+        { imageUrl && (
+          <img
+            id="inputImage"
+            alt="Input file to detect face"
+            src={imageUrl}
+            width="500px"
+            height="auto"
+          />
+        )}
         <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
       </div>
     </div>
