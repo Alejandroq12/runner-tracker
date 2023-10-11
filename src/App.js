2963c0ed4a8895b313fc46ab159ddee13a7213fc
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-10-11 15:57:08 -0600

    Feat(app.js): refactor code to implement run tracker

--- src/App.js
+++ src/App.js
@@ -2,60 +2,19 @@ import { Component } from 'react';
 import Navigation from './components/navigation/Navigation';
 import Signin from './components/signin/Signin';
 import Register from './components/register/Register';
-import FaceRecognition from './components/faceRecognition/FaceRecognition';
-import Logo from './components/logo/Logo';
-import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
-import Rank from './components/rank/Rank';
-import ParticlesBg from 'particles-bg';
+import RunTracker from './components/runTracker/RunTracker';
 import './App.css';
-
-const returnClarifaiRequestOptions = (imageUrl) => {
-  const PAT = 'eb4da108907e425c9b10c1897fffaa99';
-  const USER_ID = 'aleq12';
-  const APP_ID = 'my-first-application-eye2ra';
-  const IMAGE_URL = imageUrl;
-
-  const raw = JSON.stringify({
-    user_app_id: {
-      user_id: USER_ID,
-      app_id: APP_ID,
-    },
-    inputs: [
-      {
-        data: {
-          image: {
-            url: IMAGE_URL,
-          },
-        },
-      },
-    ],
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      Authorization: 'Key ' + PAT,
-    },
-    body: raw,
-  };
-  return requestOptions;
-};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
       route: 'signin',
       isSignedIn: false,
       user: {
         id: '',
         name: '',
         email: '',
-        entries: 0,
         joined: '',
       },
     };
@@ -67,63 +26,14 @@ class App extends Component {
         id: data.id,
         name: data.name,
         email: data.email,
-        entries: data.entries,
         joined: data.joined,
       },
     });
   };
 
   componentDidMount() {
-    fetch('http://localhost:3003/')
-      .then((response) => response.json())
+    fetch('http://localhost:3003/').then((response) => response.json());
   }
-
-  calculateFaceLocation = (data) => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
-  };
-
-  displayFaceBox = (box) => {
-    this.setState({ box: box });
-  };
-
-  onInputChange = (event) => {
-    this.setState({ input: event.target.value });
-  };
-
-  onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-    fetch(
-      `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
-      returnClarifaiRequestOptions(this.state.input)
-    )
-      .then((response) => {
-        if (response) {
-          fetch('http://localhost:3003/image', {
-            method: 'put',
-            headers: { 'Content-type': 'application/json' },
-            body: JSON.stringify({
-              id: this.state.user.id,
-            }),
-          })
-            .then((response) => response.json())
-            .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            });
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response));
-      })
-      .catch((error) => console.error('Error:', error));
-  };
 
   onRouteChange = (route) => {
     if (route === 'signout') {
@@ -135,26 +45,17 @@ class App extends Component {
   };
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    const { isSignedIn, route } = this.state;
     return (
       <div className="App">
-        <ParticlesBg num={10} type="square" bg />
         <Navigation
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
         />
         {route === 'home' ? (
           <div>
-            <Logo />
-            <Rank
-              name={this.state.user.name}
-              entries={this.state.user.entries}
-            />
-            <ImageLinkForm
-              onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
-            />
-            <FaceRecognition box={box} imageUrl={imageUrl} />
+            <h1>Run Tracker</h1>
+            <RunTracker userId={this.state.user.id} />
           </div>
         ) : route === 'signin' ? (
           <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
