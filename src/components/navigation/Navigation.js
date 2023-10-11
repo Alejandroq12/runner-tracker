34800580c9e0ce92e485bf7f8a385de5f2c3ab86
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-10-11 16:02:36 -0600

    Feat(nav bar): Improve navigation bar styling.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,10 +1,24 @@
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
-      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <nav
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          backgroundColor: '#282c34',
+          padding: '10px',
+        }}
+      >
         <p
           onClick={() => onRouteChange('signout')}
-          className="f3 link dim black underline pa3 pointer"
+          className="f3 link dim white pa3 pointer"
+          style={{
+            marginRight: '20px',
+            cursor: 'pointer',
+            transition: 'color 0.3s',
+          }}
+          onMouseOver={(e) => (e.target.style.color = 'skyblue')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
         >
           Sign Out
         </p>
@@ -12,16 +26,37 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     );
   } else {
     return (
-      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <nav
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          backgroundColor: '#282c34',
+          padding: '10px',
+        }}
+      >
         <p
           onClick={() => onRouteChange('signin')}
-          className="f3 link dim black underline pa3 pointer"
+          className="f3 link dim white pa3 pointer"
+          style={{
+            marginRight: '20px',
+            cursor: 'pointer',
+            transition: 'color 0.3s',
+          }}
+          onMouseOver={(e) => (e.target.style.color = 'skyblue')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
         >
           Sign In
         </p>
         <p
           onClick={() => onRouteChange('register')}
-          className="f3 link dim black underline pa3 pointer"
+          className="f3 link dim white pa3 pointer"
+          style={{
+            marginRight: '20px',
+            cursor: 'pointer',
+            transition: 'color 0.3s',
+          }}
+          onMouseOver={(e) => (e.target.style.color = 'skyblue')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
         >
           Register
         </p>
